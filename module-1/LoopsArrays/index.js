aa47f412bb8262613bca90223d4e3fc6e2774332
Zach Munson <zmunson85@gmail.com>
2021-09-14 01:47:35 -0600

    2partsLoopsArrays

---
+++ module-1/LoopsArrays/index.js
@@ -0,0 +1,42 @@
+const officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"];
+let count = 0;
+for (let i = 0; i < officeItems.length; i++) {
+    if (officeItems[i] === 'computer') {
+        count++
+        console.log('computer count is ' + count);
+
+    }
+}
+let peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+        name: "Mike",
+        age: 12,
+        gender: "male"
+    }, {
+        name: "Madeline",
+        age: 80,
+        gender: "female"
+    }, {
+        name: "Cheryl",
+        age: 22,
+        gender: "female"
+    }, {
+        name: "Sam",
+        age: 30,
+        gender: "male"
+    }, {
+        name: "Suzy",
+        age: 4,
+        gender: "female"
+    }
+];
+let minAge = 18;
+
+for (let j = 0; j < peopleWhoWantToSeeMadMaxFuryRoad.length; j++) {
+    if (peopleWhoWantToSeeMadMaxFuryRoad[j].age < minAge) {
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[j].name + ':' + ' Mad max is not good for your young eyes!')
+    } else (
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[j].name + ':' + ' You are an Adult, you can watch any movie you like!')
+    )
+    console.log(peopleWhoWantToSeeMadMaxFuryRoad[j].name + "'s gender is " + peopleWhoWantToSeeMadMaxFuryRoad[j].gender)
+}
