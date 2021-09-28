cf39b0bebc74dd9759846090fd92357602a4e8f5
Zach Munson <zmunson85@gmail.com>
2021-09-28 10:18:38 -0600

    'update'

--- module-2/marioPestControl/main.js
+++ module-2/marioPestControl/main.js
@@ -1,12 +1,20 @@
 function getTotal() {
-    return (document.getElementById("G-total").value * 5) +
-        (document.getElementById("BB-total").value * 7) +
-        (document.getElementById("CC-total").value * 11);
-}
+    let incValueTypeG = 5;
+    let incValueTypeB = 7;
+    let incValueTypeC = 11;
 
+    return (document.getElementById("G-total").value * incValueTypeG) +
+        (document.getElementById("BB-total").value * incValueTypeB) +
+        (document.getElementById("CC-total").value * incValueTypeC);
+}
+console.log(getTotal);
 document.getElementById("submit").addEventListener('click', function (e) {
     e.preventDefault();
     let result = document.getElementById("total").value = 'Your Price is ' + getTotal() + " Coins"
     console.log(result)
+    console.log(goomba.gTotal.value)
+    console.log(cheapCheep.cTotal.value)
+    console.log(bobOmbs.bTotal.value)
 })
 getTotal();
+
