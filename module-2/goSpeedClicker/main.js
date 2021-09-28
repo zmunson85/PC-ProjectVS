c2b34c727936965bb48d525285a7571882c0eec4
Zach Munson <zmunson85@gmail.com>
2021-09-28 00:47:43 -0600

    'local/sessionStorage'

--- module-2/goSpeedClicker/main.js
+++ module-2/goSpeedClicker/main.js
@@ -12,3 +12,18 @@ function clickCount() {
     })
 }
 clickCount();
+
+// const setCount2 = document.getElementById('count2');
+// setCount2.textContent = sessionStorage.count || 0
+
+// let counter2 = sessionStorage.count || 0
+
+// function clickCount2() {
+//     document.addEventListener('click', function () {
+//         counter2++
+//         setCount.textContent = counter;
+//         sessionStorage.setItem('count2', counter2);
+//         console.log(counter);
+//     })
+// }
+// clickCount2();
