426293bd86cbf2bda4fc2da08d7649616afd27be
Zach Munson <zmunson85@gmail.com>
2021-09-28 17:34:07 -0600

    'updateSpeedClicker'

--- module-2/goSpeedClicker/main.js
+++ module-2/goSpeedClicker/main.js
@@ -3,16 +3,42 @@ setCount.textContent = localStorage.count || 0
 
 let counter = localStorage.count || 0
 
+
 function clickCount() {
-    document.addEventListener('click', function () {
+    document.getElementById('count').addEventListener('click', function () {
         counter++
         setCount.textContent = counter;
         localStorage.setItem('count', counter);
         console.log(counter);
     })
+
 }
 clickCount();
 
+
+
+
+document.getElementById('clear').addEventListener('click', function (e) {
+    localStorage.removeItem('count');
+    counter = 0;
+    setCount.textContent = 0;
+    localStorage.clear();
+
+})
+var clickClock = 10;
+var downloadTimer = setInterval(function () {
+    if (clickClock <= 0) {
+        clearInterval(downloadTimer);
+        let endRace = document.getElementById("timer").innerHTML = "Times Up";
+        console.log(endRace);
+        localStorage.removeItem('count');
+        counter = 0;
+        setCount.textContent = 0;
+    } else {
+        document.getElementById("timer").innerHTML = clickClock + " seconds remaining";
+    }
+    clickClock -= 1;
+}, 1000);
 // const setCount2 = document.getElementById('count2');
 // setCount2.textContent = sessionStorage.count || 0
 
