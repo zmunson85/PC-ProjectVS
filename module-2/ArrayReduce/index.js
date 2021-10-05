966b796e43308b4604fd6ff3e640eacd997d7fb2
Zach Munson <zmunson85@gmail.com>
2021-10-05 02:48:19 -0600

    'updateArrayReduce'

--- module-2/ArrayReduce/index.js
+++ module-2/ArrayReduce/index.js
@@ -1,3 +1,4 @@
+console.log('_________________1________________');
 /*  Turn an array of numbers into a total of all the numbers */
 
 function total(arr) {
@@ -9,8 +10,8 @@ function total(arr) {
 console.log(total([1, 2, 3]));
 
 
+console.log('_________________2________________');
 /* Turn an array of numbers into a long string of all those numbers. */
-
 
 function stringConcat(arr) {
     return arr.reduce(function (final, num) {
@@ -21,7 +22,7 @@ function stringConcat(arr) {
 console.log(stringConcat([1, 2, 3]))
 console.log(stringConcat([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
-
+console.log('_________________3________________');
 /* Turn an array of voter objects into a count of how many people voted */
 
 function totalVotes(voters) {
@@ -51,3 +52,92 @@ var voters = [
     { name: 'Zack', age: 19, voted: false }
 ];
 console.log(totalVotes(voters)); // 7
+
+/* Given an array of all your wishlist items, figure out how much it would cost to just buy everything at once */
+console.log('_________________4________________');
+function shoppingSpree(wishlist) {
+    return wishlist.reduce(function (final, gift) {
+        return final += gift.price
+    }, 0);
+}
+
+var wishlist = [
+    { title: "Tesla Model S", price: 90000 },
+    { title: "4 carat diamond ring", price: 45000 },
+    { title: "Fancy hacky Sack", price: 5 },
+    { title: "Gold fidgit spinner", price: 2000 },
+    { title: "A second Tesla Model S", price: 90000 }
+];
+
+console.log(shoppingSpree(wishlist)); // 227005
+
+
+console.log('_________________5________________');
+/* Given an array of arrays, flaten them into a single array */
+
+function flatten(arr) {
+    return arr.flat();
+}
+
+var arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
+
+console.log('_________________6________________');
+/* 
+Include how many of the potential voters were in the ages 18-25, how many from 26-35, how many from 36-55, and how many of each of those age ranges actually voted. The resulting object containing this data should have 6 properties. See the example output at the bottom.
+Include how many of the potential voters were in the ages 18-25, how many from 26-35, how many from 36-55, and how many of each of those age ranges actually voted. The resulting object containing this data should have 6 properties. See the example output at the bottom. */
+
+var voters = [
+    { name: 'Bob', age: 30, voted: true },
+    { name: 'Jake', age: 32, voted: true },
+    { name: 'Kate', age: 25, voted: false },
+    { name: 'Sam', age: 20, voted: false },
+    { name: 'Phil', age: 21, voted: true },
+    { name: 'Ed', age: 55, voted: true },
+    { name: 'Tami', age: 54, voted: true },
+    { name: 'Mary', age: 31, voted: false },
+    { name: 'Becky', age: 43, voted: false },
+    { name: 'Joey', age: 41, voted: true },
+    { name: 'Jeff', age: 30, voted: true },
+    { name: 'Zack', age: 19, voted: false }
+];
+
+function voterResults(arr) {
+    return arr.reduce(function (final, voter) {
+        if (voter.age <= 26)
+            voter.voted ? (final.numYoungVotes++, final.numYoungPeople++) : final.numYoungPeople++;
+        else if (voter.age >= 35)
+            voter.voted ? (final.numOldVotes++, final.numOldPeople++) : final.numOldPeople++;
+        else voter.voted ? (final.numMidVotes++, final.numMidPeople++) : final.numMidPeople++;
+
+        return final;
+    }, {
+        numYoungVotes: 0,
+        numYoungPeople: 0,
+        numMidVotes: 0,
+        numMidPeople: 0,
+        numOldVotes: 0,
+        numOldPeople: 0
+    })
+}
+
+console.log(voterResults(voters)); // Returned value shown below:
+
+/* Git Hub Info */
+function getGithubInfo(zmunson85) {
+    var url = 'https://api.github.com/users/' + zmunson85;
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', url, false);
+    xmlhttp.send();
+
+    var data = xmlhttp.responseText;
+
+    console.log(data);
+}
+
