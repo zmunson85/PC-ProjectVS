f6f4770717dd01c562abee6e418404052f76247a
Zach Munson <zmunson85@gmail.com>
2021-10-07 12:56:23 -0600

    'updateCodingChallenge'

--- CodingChallenges/vowelCount.js
+++ CodingChallenges/vowelCount.js
@@ -6,28 +6,31 @@ The input string will only consist of lower case letters and/or spaces. */
 
 function getCount(str) {
 
-    //set array initial value
+    //set comparison values for testing input strings for vowels.
     let arr = ['a', 'e', 'i', 'o', 'u'];
 
+    //initial count value is set to zero, waiting on loops to find a count of vowels in each string
     var vowelsCount = 0;
 
     //loop through the array by targeting length
-    //also loop throguh the count, nested loop. 
+    //also loop through the count, nested loop. 
     for (let i = 0; i < arr.length; i++) {
+
+        //loop the str length and compare to arr values.
         for (let j = 0; j < str.length; j++) {
-            //compare the count to index in array increase the count
+
+            //compare the count to index in array increase the count of characters in each string.
             if (str[j] === arr[i]) {
-                console.log(vowelsCount)
+                //count before
+                console.log(str)
                 vowelsCount++;
                 console.log(vowelsCount)
             }
         }
     }
-    // enter your majic here
-
-    return console.log(vowelsCount);
+    return vowelsCount;
 }
-/* the strings are counted and value is returned with each string parameter. */
+/* each string char is counted and compared to reference array, if a vowel is present the count will increase. Return total vowels in each string input */
 
 getCount('abracadabra')
 getCount('pear tree')
