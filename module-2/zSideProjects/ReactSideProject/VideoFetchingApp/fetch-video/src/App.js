b07e78cda707783cce87561d2ada70b73104d582
Zach Munson <zmunson85@gmail.com>
2021-10-07 03:36:47 -0600

    'update/sideProject

---
+++ module-2/zSideProjects/ReactSideProject/VideoFetchingApp/fetch-video/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import Youtube from './Youtube';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>Fetch Youtube Videos By Channel </h2>
+        </div>
+        <Youtube />
+      </div>
+    );
+  }
+}
+
+export default App;
