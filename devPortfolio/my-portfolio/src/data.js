969901c4fd44aff8831f66ad9a884ee9acef0123
Zach Munson <zmunson85@gmail.com>
2021-12-08 16:43:28 -0700

    'update'

---
+++ devPortfolio/my-portfolio/src/data.js
@@ -0,0 +1,82 @@
+export const projects = [
+  {
+    title: "Axios To Do List",
+    subtitle: "Vanilla JavaScript",
+    description:
+      "Demonstrating my ability to create a CRUD app with axios requests. Deployed to netlify.",
+    image: "./axios-todo.gif",
+    link: "https://vsaxiostodolist.netlify.app",
+  },
+  {
+    title: "Khan Ban App",
+    subtitle: "Local Storage,Vanilla JavaScript",
+    description:
+      "Demonstrating the use of local storage and an ability to drag and drop items within the khan ban catagories. ",
+    image: "./khanBan.gif",
+    link: "https://dragndropkhanban.netlify.app",
+  },
+  {
+    title: "Meme Generator",
+    subtitle: "React Crud Application, imgFLip Api",
+    description:
+      "Stop Stealing My Memes, Create Your Own",
+    image: "./meme-generator.gif",
+    link: "https://cruddy-meme-generator-vschool.netlify.app",
+  },
+  {
+    title: "Cocktails Api",
+    subtitle: "React App Using Hooks",
+    description:
+      "Fun interactive app you can explore new cocktails, maybe try them out at the local pub!",
+    image: "../cocktails.gif",
+    link: "https://drink-mixer.netlify.app/",
+  },
+  {
+    title: "Movies DB",
+    subtitle: "React App using omdb APi with Hooks",
+    description:
+      "Using an api to populate data for display only, using react router to navigate views on app",
+    image: "./moviesDb.gif",
+    link: "https://cap4-movies-db.netlify.app/",
+  },
+  {
+    title: "React Todo List",
+    subtitle: "React App, With Hooks, Material UI",
+    description:
+      "Todo app with interactive options using Styled Components",
+    image: "./react-todo.gif",
+    link: "https://drink-mixer.netlify.app/home",
+
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Data Structures",
+  "Python",
+  "Django",
+  "JavaScript",
+  "React",
+  "HTML",
+  "Tailwind CSS",
+  "Boot-Strap",
+  "MERN",
+  "C# ASP.NET",
+
+];
